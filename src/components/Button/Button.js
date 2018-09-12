d4393f529a5ee41fd252f71185bcfe4b6693d21b
Connor Wilson <connor.wilson@tulip.io>
2018-09-11 20:43:30 -0400

    Document the reasonable files

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,6 +1,11 @@
 import React from 'react';
+// Styled components 💅 is library that allows you create components easily and
+// style them, css-in-js style. It's totally optional, but I prefer to use it
+// to build things quickly and have them look great every step of the way.
 import styled from 'styled-components';
 
+// Style components take CSS in a template string. Even Sass functions with work!
+// Each element is a property of styled, like h3, p, div, etc...
 const ButtonWrapper = styled.button`
 	border-radius: 8px;
 	color: #fff;
@@ -10,8 +15,13 @@ const ButtonWrapper = styled.button`
 	outline: none;
 `;
 
+// Components are functions, and they must start with a capital letter
 function Button(props) {
+	// {...props} uses the the ES6 spread operator to send any props you may pass
+	// along without changing any of the contents. This is basically just creating
+	// a copy to pass along
 	return <ButtonWrapper {...props}>{props.children}</ButtonWrapper>;
 }
 
+// This export will be picked up in ./index.js
 export default Button;
--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,3 +1,4 @@
+// Import the Button component from this folder and send it down to ./components/index.js
 import Button from './Button';
 
 export default Button;
