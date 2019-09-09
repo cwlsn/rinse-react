eba13a455beffa5dc1c881b8c79474b5f15f5963
Kunal Marwaha <marwahaha@berkeley.edu>
2019-09-08 20:04:18 -0700

    typo: explicity -> explicitly

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,4 @@
-// In this file we explicity export everything. This is just to be thorough
+// In this file we explicitly export everything. This is just to be thorough
 // and explicit. This thorough exporting method can seem like a lot, but it
 // allows for simpler scaling when your library grows in size, and even adds
 // different tech like TypeScript
